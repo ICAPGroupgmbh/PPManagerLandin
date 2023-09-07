bd3fb8af7640305ea926b0c4563e791387065e27
akiselevych <anton.kiselevych@icapgroupgmbh.com>
2023-09-07 17:22:33 +0300

    new build

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -111,35 +111,8 @@ menuLink.forEach(button => {
 
 const mainIndex = document.querySelector(".index-main");
 
-const acceptCookiesHandler = () => {
-    document.querySelector(".banner__wrapper").classList.add("disabled");
-    document.querySelector(".banner2__wrapper").classList.add("active");
-}
 const closeCookiesHandler = () => {
     document.querySelector(".banner__wrapper").classList.add("disabled");
-    document.querySelector('.overlay').classList.remove("active");
-    localStorage.setItem("protectionCookie", JSON.stringify(true));
-}
-
-const acceptProtectionHandler = () => {
-    document.querySelector(".banner2__wrapper").classList.add("disabled");
-    document.querySelector(".banner3__wrapper").classList.add("active");
-}
-const closeProtectionHandler = () => {
-    document.querySelector(".banner2__wrapper").classList.remove("active");
-    document.querySelector('.overlay').classList.remove("active");
-    localStorage.setItem("protectionCookie", JSON.stringify(true));
-}
-
-const acceptLearnMoreHandler = () => {
-    document.querySelector(".protection-main").classList.add("active");
-    document.querySelector(".index-main").classList.remove("active");
-    document.querySelector(".banner3__wrapper").classList.remove("active");
-    document.querySelector('.overlay').classList.remove("active");
-    localStorage.setItem("protectionCookie", JSON.stringify(true));
-}
-const closeLearnMoreHandler = () => {
-    document.querySelector(".banner3__wrapper").classList.remove("active");
     document.querySelector('.overlay').classList.remove("active");
     localStorage.setItem("protectionCookie", JSON.stringify(true));
 }
@@ -163,43 +136,9 @@ window.addEventListener("load", () => {
                 </div>
               `
         )
-        document.querySelector(".banner__main-btn").addEventListener("click", acceptCookiesHandler);
+        document.querySelector(".banner__main-btn").addEventListener("click", closeCookiesHandler);
         document.querySelector(".banner__secondary-btn").addEventListener("click", closeCookiesHandler);
 
-        mainIndex.insertAdjacentHTML("beforebegin",
-            `
-            <div class="banner2__wrapper">
-                <div class="banner2__title">Schutz Ihrer Daten</div>
-                <div class="banner2__desc">Ihre Privatsphäre ist uns wichtig. Diese Website verpflichtet sich, Ihre persönlichen Daten zu schützen.</div>
-                <div class="banner2__text">Seien Sie versichert, dass Ihre Privatsphäre und die Sicherheit Ihrer Daten bei uns an erster Stelle stehen. In unserer Datenschutzrichtlinie erfahren Sie, wie wir Ihre Daten erfassen, verwenden und schützen. Wenn Sie auf "Mehr erfahren" klicken oder diese Website weiter nutzen, erklären Sie sich mit unserer Datenschutzrichtlinie einverstanden.</div>
-                    <div class="banner2__btn-container">
-                        <div class="banner2__main-btn">Mehr erfahren</div>
-                        <div class="banner2__secondary-btn">Schließen</div>
-                    </div>
-            </div>
-                `
-        )
-        document.querySelector(".banner2__main-btn").addEventListener("click", acceptProtectionHandler);
-        document.querySelector(".banner2__secondary-btn").addEventListener("click", closeProtectionHandler);
-
-        mainIndex.insertAdjacentHTML("beforebegin",
-            `
-            <div class="banner3__wrapper">
-                <div class="banner3__title">Schutz Ihrer Daten</div>
-                <div class="banner3__text">Besuchen Sie unsere <span class="banner3__link">Datenschutzrichtlinie</span>,
-                    um mehr darüber
-                    zu erfahren, wie wir mit Ihren Daten
-                    umgehen, welche Rechte Sie haben und wie Sie uns bei datenschutzrechtlichen Bedenken kontaktieren können.</div>
-                <div class="banner3__btn-container">
-                    <div class="banner3__main-btn">Mehr erfahren</div>
-                    <div class="banner3__secondary-btn">Schließen</div>
-                </div>
-            </div>
-                `
-        )
-        document.querySelector(".banner3__main-btn").addEventListener("click", acceptLearnMoreHandler);
-        document.querySelector(".banner3__link").addEventListener("click", acceptLearnMoreHandler);
-        document.querySelector(".banner3__secondary-btn").addEventListener("click", closeLearnMoreHandler);
     }
 })
 
